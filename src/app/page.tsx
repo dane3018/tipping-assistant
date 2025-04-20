12f3aaf19a43077cf9c8bc4d7527cba94b242576
Dane Osborne <dane3018@gmail.com>
2025-04-20 21:51:33 +1000

    Added shadn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,85 @@
-import Image from "next/image";
-import Link from "next/link";
+"use client"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
-export default function Home() {
+import GameList from "./GameCard"
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@radix-ui/react-collapsible"
+import React from "react"
+import { Button } from "@/components/ui/button"
+import { ChevronsUpDown, Ghost } from "lucide-react"
+
+const mockGames = [
+  {
+    id: 1,
+    date: "2025-04-18T19:50:00Z",
+    venue: "MCG",
+    home_team: {
+      id: 1,
+      name: "Richmond",
+      ladderPosition: 6,
+      lastFive: ["W", "L", "W", "W", "L"],
+    },
+    away_team: {
+      id: 2,
+      name: "Carlton",
+      ladderPosition: 3,
+      lastFive: ["W", "W", "W", "L", "W"],
+    },
+  },
+]
+
+export default function Page() {
+  const [isOpen, setIsOpen] = React.useState(false)
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="/teams"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            View all teams
-          </Link>
-          <Link
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="/games"
-          >
-            Current round
-          </Link>
+    <div className="flex border border-gray-300 rounded-lg shadow-md p-4 flex-col justify-between m-8">
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg font-semibold">Richmond</h2>
+        <span className="text-sm text-gray-500">12th April</span>
+        <h2 className="text-lg font-semibold">Collingwood</h2>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-        <Link href={"/teams"}>teams here</Link>
-      </footer>
-    </div>
-  );
+    
+    <Collapsible 
+      open={isOpen}
+      onOpenChange={setIsOpen}
+      >
+        <CollapsibleTrigger asChild>
+        <Button variant={"ghost"} size={"default"}>
+        <span>More stats</span>
+        <ChevronsUpDown className="h-4 w-4" />
+        
+        </Button>
+        </CollapsibleTrigger>
+        <CollapsibleContent>
+        <div className="">
+          <div className="rounded-lg shadow-md border-2 my-2">
+            <h2>Last 5 matches</h2>
+            <ol>
+              <li>Richmond</li>
+              <li>Collingwood</li>
+              <li>Richmond</li>
+              <li>Richmond</li>
+              <li>Richmond</li>
+            </ol>
+          </div>
+          <div className="rounded-lg shadow-md border-2 my-2">
+            <h2>Last 5 head to head matches </h2>
+            
+          </div>
+        </div>
+        </CollapsibleContent>
+    </Collapsible>
+
+  </div>
+
+  )
+  
+  
 }
