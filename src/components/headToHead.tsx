07833dbcb61e4ce636bd2911efdfa0adae66602a
Dane Osborne <dane3018@gmail.com>
2025-04-21 18:07:54 +1000

    added logos

--- src/components/headToHead.tsx
+++ src/components/headToHead.tsx
@@ -8,14 +8,15 @@ interface h2hProps {
 
 export default function HeadToHead(props: h2hProps) {
   return (
-    <div className="shadow-2xs border-t-2 border-x-2">
+    <div className="shadow-2xs border-t-2 border-x-2 p-2">
       <h2 className="flex justify-center">Last 5 head to head matches </h2>
-      <div className="flex justify-center">
+      <div className="flex justify-center pt-2">
         {props.h2h.map((homeWin, index) => (
           <img
             key={index}
             src={homeWin ? props.hteamImgPath : props.ateamImgPath}
             alt={homeWin ? props.hteamName : props.ateamName}
+            className="w-18 h-18 object-contain mx-2"
           />
         ))}
       </div>
