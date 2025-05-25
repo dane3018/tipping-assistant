c3fe4b5ae3ac0b7fea641860e0d8b09984e97db0
Dane Osborne <dane3018@gmail.com>
2025-05-25 17:49:57 +1000

    Made some progress on update edge function

--- supabase/functions/update-upcoming-games/index.ts
+++ supabase/functions/update-upcoming-games/index.ts
@@ -4,20 +4,79 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { createClient } from 'jsr:@supabase/supabase-js@2'
+
+Deno.serve(async (req) => {
+  try {
+    const supabase = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+
+    const { data, error } = await supabase.from('settings').select("value").eq("id", "currentRound").single()
+
+    if (error) {
+      throw error
+    }
+    const curRound : number = +data.value;
+    const nextRound = curRound + 1;
+
+    const apiUrl = `https://api.squiggle.com.au/?q=games;year=2025;round=${nextRound}`;
+    const apiResponse = await fetch(apiUrl);
+    if (!apiResponse.ok) {
+      console.error("Error fetching Squiggle API data");
+      return new Response("Failed to fetch Squiggle API", { status: 500 });
+    }
+    const apiJson = await apiResponse.json();
+    const games = apiJson.games;
+    const returnGames = []
+
+    for (let i = 0; i < games.length; i++) {
+      const curGame = games[i]
+      const updatedGame = {
+        ascore: curGame.ascore,
+        hscore: curGame.hscore,
+        agoals: curGame.agoals,
+        abehinds: curGame.abehinds,
+        hgoals: curGame.hgoals,
+        hbehinds: curGame.hbehinds,
+        complete: curGame.complete,
+        winnerteamid: curGame.winnerteamid
+      }
+      returnGames.push(updatedGame)
+    }
+
+
+    // const { error: updateEr } = await supabase.from("games").update({
+      
+    // }).eq(, nextRound)
+
+    return new Response(JSON.stringify({ returnGames }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (err) {
+    return new Response(String(err?.message ?? err), { status: 500 })
+  }
+})
 
 console.log("Hello from Functions!")
 
-Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
-  }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
-})
+// Deno.serve(async (req) => {
+//   const { name } = await req.json()
+//   const data = {
+//     message: `Hello ${name}!`,
+//   }
+
+
+
+//   return new Response(
+//     JSON.stringify(data),
+//     { headers: { "Content-Type": "application/json" } },
+//   )
+// })
 
 /* To invoke locally:
 
