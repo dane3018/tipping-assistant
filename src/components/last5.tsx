660ecf4e38c3469775c4c7c0048e7d0fbbb8159a
Dane Osborne <dane3018@gmail.com>
2025-06-07 10:00:50 +1000

    Fixed title centering issue in last5

--- src/components/last5.tsx
+++ src/components/last5.tsx
@@ -14,34 +14,42 @@ export default function Last5(props: last5Props) {
   ]);
 
   return (
-    <div className="shadow-2xs border-t-2 border-x-2">
-      <div className="flex justify-between items-center">
-        <div className="flex justify-center">
-          {props.hteamWins.map((res, index) => (
-            <div
-              key={index}
-              className={`w-4 h-4 p-4 m-4 flex items-center justify-center rounded-md ${resToColour.get(
-                res,
-              )}`}
-            >
-              <p className="font-bold text-xl text-gray-50">{res}</p>
-            </div>
-          ))}
-        </div>
-        <p className="">{props.title}</p>
-        <div className="flex justify-center">
-          {props.ateamWins.map((res, index) => (
-            <div
-              key={index}
-              className={`w-4 h-4 p-4 m-4 flex items-center justify-center rounded-md ${resToColour.get(
-                res,
-              )}`}
-            >
-              <p className="font-bold text-xl text-gray-50">{res}</p>
-            </div>
-          ))}
-        </div>
+  <div className="shadow-2xs border-t-2 border-x-2 relative">
+    <div className="flex justify-between items-center relative">
+      {/* Centered Title */}
+      <p className="absolute left-1/2 transform -translate-x-1/2">
+        {props.title}
+      </p>
+
+      {/* Home team wins */}
+      <div className="flex justify-center">
+        {props.hteamWins.map((res, index) => (
+          <div
+            key={index}
+            className={`w-4 h-4 p-4 m-4 flex items-center justify-center rounded-md ${resToColour.get(
+              res,
+            )}`}
+          >
+            <p className="font-bold text-xl text-gray-50">{res}</p>
+          </div>
+        ))}
+      </div>
+
+      {/* Away team wins */}
+      <div className="flex justify-center">
+        {props.ateamWins.map((res, index) => (
+          <div
+            key={index}
+            className={`w-4 h-4 p-4 m-4 flex items-center justify-center rounded-md ${resToColour.get(
+              res,
+            )}`}
+          >
+            <p className="font-bold text-xl text-gray-50">{res}</p>
+          </div>
+        ))}
       </div>
     </div>
-  );
+  </div>
+);
+
 }
